18d66d5561eec62445f844c61605408a4ef8097b
Felipe Simis <felipee.oliveira17@hotmail.com>
2024-09-26 11:42:07 -0300

    test: ensure Resize method changes dimensions correctly

--- internal/image/bimg_image_test.go
+++ internal/image/bimg_image_test.go
@@ -23,3 +23,16 @@ func TestBimgImageWrapper_Size(t *testing.T) {
 	assert.Equal(t, 600, size.Width)
 	assert.Equal(t, 400, size.Height)
 }
+
+func TestBimgImageWrapper_Resize(t *testing.T) {
+	img := NewBimgImage(mockedImage())
+	resized, err := img.Resize(300, 200)
+
+	assert.Nil(t, err)
+	assert.NotEmpty(t, resized)
+
+	size, err := NewBimgImage(resized).Size()
+	assert.Nil(t, err)
+	assert.Equal(t, 300, size.Width)
+	assert.Equal(t, 200, size.Height)
+}
