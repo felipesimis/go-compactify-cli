b2a79fb3792ceb374a82d900777ff1daec970e35
Felipe Simis <felipee.oliveira17@hotmail.com>
2024-09-26 15:52:38 -0300

    feat: ensure FormatValidation return an error on invalid format

--- pkg/validation/format_validation.go
+++ pkg/validation/format_validation.go
@@ -3,15 +3,26 @@ package validation
 import "errors"
 
 var (
-	ErrInvalidFormat = errors.New("invalid format")
+	ErrFormatRequired = errors.New("format is required")
+	ErrInvalidFormat  = errors.New("invalid format")
 )
 
 type FormatValidation struct {
 	Format string
 }
 
+var supportedFormats = map[string]bool{
+	"jpg":  true,
+	"jpeg": true,
+	"png":  true,
+	"webp": true,
+}
+
 func (f *FormatValidation) Validate() error {
 	if f.Format == "" {
+		return ErrFormatRequired
+	}
+	if _, ok := supportedFormats[f.Format]; !ok {
 		return ErrInvalidFormat
 	}
 	return nil
