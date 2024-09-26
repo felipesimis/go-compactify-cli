095f726f3807c3470088b1b37ad2c4f1272999df
Felipe Simis <felipee.oliveira17@hotmail.com>
2024-09-26 12:39:26 -0300

    feat: ensure ReadDir method correctly identifies image files

---
+++ internal/filesystem/filesystem.go
@@ -0,0 +1,39 @@
+package filesystem
+
+import (
+	"os"
+	"path/filepath"
+
+	"github.com/felipesimis/compactify-cli/internal/utils"
+)
+
+type FileSystem interface {
+	ReadDir(path string) ([]string, error)
+}
+
+type FileSystemWrapper struct{}
+
+func NewFileSystem() FileSystem {
+	return &FileSystemWrapper{}
+}
+
+func (fs *FileSystemWrapper) ReadDir(path string) ([]string, error) {
+	dir, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer dir.Close()
+
+	fileInfos, err := dir.Readdir(-1)
+	if err != nil {
+		return nil, err
+	}
+
+	var files []string
+	for _, fi := range fileInfos {
+		if !fi.IsDir() && utils.IsValidImage(fi.Name()) {
+			files = append(files, filepath.Join(path, fi.Name()))
+		}
+	}
+	return files, nil
+}
