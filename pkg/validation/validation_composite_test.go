d6bc54f9833c94bc1f63ef7999ce6ac99bd0b60a
Felipe Simis <felipee.oliveira17@hotmail.com>
2024-09-26 16:46:41 -0300

    test: ensure ValidationComposite returns nil on Validate success

--- pkg/validation/validation_composite_test.go
+++ pkg/validation/validation_composite_test.go
@@ -42,3 +42,14 @@ func TestValidationComposite_MultipleErrors(t *testing.T) {
 	assert.Equal(t, "any_error", err.Error())
 	assert.NotEqual(t, ErrMock, err)
 }
+
+func TestValidationComposite_Success(t *testing.T) {
+	validationStub := ValidationComposite{
+		Validations: []Validation{
+			MockValidator{Err: nil},
+		},
+	}
+
+	err := validationStub.Validate()
+	assert.Nil(t, err)
+}
