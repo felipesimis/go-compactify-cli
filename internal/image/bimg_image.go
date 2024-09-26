ba45ed0e5267dc6e76583f3f8b22bdae6331db91
Felipe Simis <felipee.oliveira17@hotmail.com>
2024-09-26 11:42:19 -0300

    feat: ensure Resize method changes dimensions correctly

--- internal/image/bimg_image.go
+++ internal/image/bimg_image.go
@@ -3,6 +3,7 @@ package image
 import "github.com/h2non/bimg"
 
 type BimgImage interface {
+	Resize(width, height int) ([]byte, error)
 	Size() (bimg.ImageSize, error)
 }
 
@@ -16,4 +17,8 @@ func NewBimgImage(buffer []byte) BimgImage {
 
 func (b *BimgImageWrapper) Size() (bimg.ImageSize, error) {
 	return b.image.Size()
+}
+
+func (b *BimgImageWrapper) Resize(width, height int) ([]byte, error) {
+	return b.image.Resize(width, height)
 }
