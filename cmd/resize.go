387f725d8977352c8ac85e26007184d309dc5dee
Felipe Simis <felipee.oliveira17@hotmail.com>
2024-09-27 17:21:45 -0300

    feat: implement resize command to resize images to specified dimensions

--- cmd/resize.go
+++ cmd/resize.go
@@ -5,7 +5,16 @@ package cmd
 
 import (
 	"fmt"
+	"log"
+	"os"
+	"sync"
+	"sync/atomic"
 
+	"github.com/felipesimis/compactify-cli/internal/filesystem"
+	"github.com/felipesimis/compactify-cli/internal/image"
+	"github.com/felipesimis/compactify-cli/internal/utils"
+	"github.com/felipesimis/compactify-cli/pkg/progress"
+	"github.com/felipesimis/compactify-cli/pkg/validation"
 	"github.com/spf13/cobra"
 )
 
@@ -15,8 +24,123 @@ var (
 	height    int
 )
 
+type ProcessFileParams struct {
+	fileInfo      filesystem.FileInfo
+	fs            filesystem.FileSystem
+	outputDir     string
+	initialSize   *uint64
+	finalSize     *uint64
+	skippedImages *uint32
+	resizedImages *uint32
+	errChan       chan error
+	width         int
+	height        int
+	progressBar   *progress.ProgressBar
+}
+
 func resizeRun(cmd *cobra.Command, args []string) {
-	fmt.Println("resize called")
+	fs := filesystem.NewFileSystem()
+	files, err := fs.ReadDir(directory)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	outputDir, err := fs.CreateSiblingDir(directory, "-resized")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	sem := make(chan struct{}, concurrency)
+	errChan := make(chan error, len(files))
+	var wg sync.WaitGroup
+	var initialSize, finalSize uint64
+	var skippedImages, resizedImages uint32
+
+	rb := utils.NewResultBuilder(utils.RealTimeProvider{})
+	progressBar := progress.NewProgressBar(os.Stdout, len(files), "Resizing images")
+
+	for _, fileInfo := range files {
+		wg.Add(1)
+		sem <- struct{}{}
+		go func(fileInfo filesystem.FileInfo) {
+			defer func() {
+				<-sem
+				wg.Done()
+			}()
+			params := ProcessFileParams{
+				fileInfo:      fileInfo,
+				fs:            fs,
+				outputDir:     outputDir,
+				initialSize:   &initialSize,
+				finalSize:     &finalSize,
+				skippedImages: &skippedImages,
+				resizedImages: &resizedImages,
+				errChan:       errChan,
+				width:         width,
+				height:        height,
+				progressBar:   progressBar,
+			}
+			processFile(params)
+		}(fileInfo)
+	}
+
+	wg.Wait()
+	close(errChan)
+
+	progressBar.Finish()
+
+	for err := range errChan {
+		log.Println(err)
+	}
+
+	totalImages := uint32(len(files))
+	rb.SetTotalImages(totalImages).
+		SetSkippedImages(skippedImages).
+		SetProcessedImages(resizedImages).
+		SetOutputDirectory(outputDir).
+		SetInitialSize(float64(initialSize)).
+		SetFinalSize(float64(finalSize))
+	result := rb.Build()
+	fmt.Println(result.PrintResults("resized"))
+}
+
+func processFile(params ProcessFileParams) {
+	dimensionValidation := &validation.DimensionsValidation{Width: params.width, Height: params.height}
+	err := dimensionValidation.Validate()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	img, err := params.fs.ReadFile(params.fileInfo.Path)
+	if err != nil {
+		params.errChan <- err
+		atomic.AddUint32(params.skippedImages, 1)
+		params.progressBar.Increment()
+		return
+	}
+
+	atomic.AddUint64(params.initialSize, uint64(params.fileInfo.Size))
+	newImg := image.NewBimgImage(img)
+	resizedImg, err := newImg.Resize(params.width, params.height)
+	if err != nil {
+		params.errChan <- err
+		atomic.AddUint32(params.skippedImages, 1)
+		params.progressBar.Increment()
+		return
+	}
+
+	outputPath := utils.BuildOutputPath(params.outputDir, params.fileInfo.Path)
+	err = params.fs.WriteFile(outputPath, resizedImg)
+	if err != nil {
+		params.errChan <- err
+		atomic.AddUint32(params.skippedImages, 1)
+		params.progressBar.Increment()
+		return
+	}
+
+	atomic.AddUint64(params.finalSize, uint64(len(resizedImg)))
+	atomic.AddUint32(params.resizedImages, 1)
+	params.progressBar.Increment()
 }
 
 var resizeCmd = &cobra.Command{
@@ -36,6 +160,7 @@ func init() {
 	resizeCmd.Flags().StringVarP(&directory, "directory", "d", "", "Directory containing the image to resize")
 	resizeCmd.Flags().IntVarP(&width, "width", "w", 0, "Desired width of the image")
 	resizeCmd.Flags().IntVarP(&height, "height", "H", 0, "Desired height of the image")
+	resizeCmd.Flags().IntVarP(&concurrency, "concurrency", "c", 20, "Number of concurrent operations")
 
 	resizeCmd.MarkFlagRequired("directory")
 	resizeCmd.MarkFlagRequired("width")
