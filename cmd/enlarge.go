683a9a52646b537ce2cb3dfdaaaf206c982215bc
Felipe Simis <felipee.oliveira17@hotmail.com>
2024-10-25 16:04:03 -0300

    feat: add enlarge command

---
+++ cmd/enlarge.go
@@ -0,0 +1,145 @@
+package cmd
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"sync/atomic"
+
+	"github.com/felipesimis/compactify-cli/internal/filesystem"
+	"github.com/felipesimis/compactify-cli/internal/image"
+	"github.com/felipesimis/compactify-cli/internal/processing"
+	"github.com/felipesimis/compactify-cli/internal/utils"
+	"github.com/felipesimis/compactify-cli/pkg/progress"
+	"github.com/felipesimis/compactify-cli/pkg/validation"
+	"github.com/spf13/cobra"
+)
+
+type EnlargeParams struct {
+	Width  int
+	Height int
+}
+
+type EnlargeStats struct {
+	initialSize    *uint64
+	finalSize      *uint64
+	skippedImages  *uint32
+	enlargedImages *uint32
+}
+
+func enlargeRun(cmd *cobra.Command, args []string) {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	dimensionValidation := &validation.DimensionsValidation{Width: width, Height: height}
+	err := dimensionValidation.Validate()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fs := filesystem.NewFileSystem()
+	files, err := fs.ReadDir(directory)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	outputDir, err := fs.CreateSiblingDir(directory, fmt.Sprintf("-enlarged_%dx%d", width, height))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var initialSize, finalSize uint64
+	var skippedImages, enlargedImages uint32
+
+	resultBuilder := utils.NewResultBuilder(utils.RealTimeProvider{})
+	progressBar := progress.NewProgressBar(os.Stdout, len(files), concurrency, "Enlarging images")
+
+	params := processing.ProcessFilesParams{
+		Files:       files,
+		FS:          fs,
+		OutputDir:   outputDir,
+		ProgressBar: progressBar,
+		ExtraParams: EnlargeParams{Width: width, Height: height},
+		ProcessorFunc: func(p processing.FileProcessingParams) error {
+			extraParams := p.ExtraParams.(EnlargeParams)
+			stats := &EnlargeStats{
+				initialSize:    &initialSize,
+				finalSize:      &finalSize,
+				skippedImages:  &skippedImages,
+				enlargedImages: &enlargedImages,
+			}
+			return enlargeImages(ctx, p, extraParams, stats)
+		},
+		Concurrency: concurrency,
+	}
+	processing.ProcessFiles(params)
+
+	progressBar.Finish()
+
+	totalImages := uint32(len(files))
+	resultBuilder.SetTotalImages(totalImages).
+		SetSkippedImages(skippedImages).
+		SetProcessedImages(enlargedImages).
+		SetOutputDirectory(outputDir).
+		SetInitialSize(float64(initialSize)).
+		SetFinalSize(float64(finalSize))
+	result := resultBuilder.Build()
+	fmt.Println(result.PrintResults("enlarged"))
+}
+
+func enlargeImages(ctx context.Context, params processing.FileProcessingParams, extraParams EnlargeParams, stats *EnlargeStats) error {
+	select {
+	case <-ctx.Done():
+		return ctx.Err()
+	default:
+	}
+
+	img, err := params.FS.ReadFile(params.File.Path)
+	if err != nil {
+		atomic.AddUint32(stats.skippedImages, 1)
+		return err
+	}
+
+	atomic.AddUint64(stats.initialSize, uint64(params.File.Size))
+	newImg := image.NewBimgImage(img)
+	enlargedImg, err := newImg.Enlarge(extraParams.Width, extraParams.Height)
+	if err != nil {
+		atomic.AddUint32(stats.skippedImages, 1)
+		return err
+	}
+
+	outputPath := utils.BuildOutputPath(params.OutputDir, params.File.Path)
+	err = params.FS.WriteFile(outputPath, enlargedImg)
+	if err != nil {
+		atomic.AddUint32(stats.skippedImages, 1)
+		return err
+	}
+
+	atomic.AddUint64(stats.finalSize, uint64(len(enlargedImg)))
+	atomic.AddUint32(stats.enlargedImages, 1)
+	return nil
+}
+
+var enlargeCmd = &cobra.Command{
+	Use:   "enlarge",
+	Args:  cobra.NoArgs,
+	Short: "Enlarge an image to specified dimensions while maintaining aspect ratio",
+	Long: `Enlarge an image to a specific width and height while maintaining the aspect ratio.
+This command allows you to change the dimensions of an image, which can be useful for optimizing images for 
+different uses, such as web, mobile, or print. You can specify the desired width and height, 
+and the image will be enlarged accordingly, keeping its original aspect ratio.`,
+	Run: enlargeRun,
+}
+
+func init() {
+	rootCmd.AddCommand(enlargeCmd)
+
+	enlargeCmd.Flags().StringVarP(&directory, "directory", "d", "", "Directory containing the images to enlarge")
+	enlargeCmd.Flags().IntVarP(&width, "width", "w", 0, "Desired width of the image")
+	enlargeCmd.Flags().IntVarP(&height, "height", "H", 0, "Desired height of the image")
+
+	enlargeCmd.MarkFlagRequired("directory")
+	enlargeCmd.MarkFlagRequired("width")
+	enlargeCmd.MarkFlagRequired("height")
+}
