342c35a9661b63de28f53c7e34b962d28ed3bf0b
Felipe Simis <felipee.oliveira17@hotmail.com>
2024-10-25 13:43:23 -0300

    test: ensure GravityValidation returns nil on Validate success

--- pkg/validation/gravity_validation_test.go
+++ pkg/validation/gravity_validation_test.go
@@ -14,3 +14,12 @@ func TestGravityValidation_Validate_Error(t *testing.T) {
 	assert.Equal(t, ErrInvalidGravity, err)
 	assert.Equal(t, ErrInvalidGravity, err2)
 }
+
+func TestGravityValidation_Validate_Success(t *testing.T) {
+	v := &GravityValidation{Gravity: 0}
+	err := v.Validate()
+	v2 := &GravityValidation{Gravity: 5}
+	err2 := v2.Validate()
+	assert.Nil(t, err)
+	assert.Nil(t, err2)
+}
