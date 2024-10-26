3e40b3a376dbb9401ee9df0bbdb0346635db0831
Felipe Simis <felipee.oliveira17@hotmail.com>
2024-10-26 11:58:15 -0300

    test: ensure WidthValidation returns an error if width is above the maximum allowed value

--- pkg/validation/width_validation_test.go
+++ pkg/validation/width_validation_test.go
@@ -11,3 +11,9 @@ func TestWidthValidation_ErrWidthSmall(t *testing.T) {
 	err := w.Validate()
 	assert.Equal(t, ErrWidthTooSmall, err)
 }
+
+func TestWidthValidation_ErrWidthLarge(t *testing.T) {
+	w := WidthValidation{Width: 100, MaxWidth: 50}
+	err := w.Validate()
+	assert.Equal(t, ErrWidthTooLarge, err)
+}
