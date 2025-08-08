1ac6605339a68255cdf7acba3cd64dd2b76891a4
Felipe Simis <felipee.oliveira17@hotmail.com>
2025-08-07 21:24:42 -0300

    refactor: use generic processing helper in grayscale command

--- cmd/grayscale.go
+++ cmd/grayscale.go
@@ -2,16 +2,11 @@ package cmd
 
 import (
 	"context"
-	"fmt"
-	"log"
-	"os"
-	"sync/atomic"
 
 	"github.com/felipesimis/compactify-cli/internal/filesystem"
 	"github.com/felipesimis/compactify-cli/internal/image"
 	"github.com/felipesimis/compactify-cli/internal/processing"
 	"github.com/felipesimis/compactify-cli/internal/utils"
-	"github.com/felipesimis/compactify-cli/pkg/progress"
 	"github.com/spf13/cobra"
 )
 
@@ -20,79 +15,22 @@ func grayscaleRun(cmd *cobra.Command, args []string) {
 	defer cancel()
 
 	fs := filesystem.NewFileSystem()
-	files, err := fs.ReadDir(directory)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	outputDir, err := fs.CreateSiblingDir(directory, "-grayscale")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	var initialSize, finalSize uint64
-	var skippedImages, grayscaleImages uint32
-
-	resultBuilder := utils.NewResultBuilder(utils.RealTimeProvider{})
-	progressBar := progress.NewProgressBar(os.Stdout, len(files), concurrency, "Grayscaling images")
-
-	params := processing.ProcessFilesParams{
-		Files:       files,
-		FS:          fs,
-		OutputDir:   outputDir,
-		ProgressBar: progressBar,
-		ProcessorFunc: func(p processing.FileProcessingParams) error {
-			stats := utils.NewImageProcessingStats(&initialSize, &finalSize, &skippedImages, &grayscaleImages)
-			return processGrayscaleImage(ctx, p, stats)
-		},
-		Concurrency: concurrency,
-	}
-	processing.ProcessFiles(params)
-
-	progressBar.Finish()
-
-	totalImages := uint32(len(files))
-	resultBuilder.SetTotalImages(totalImages).
-		SetSkippedImages(skippedImages).
-		SetProcessedImages(grayscaleImages).
-		SetOutputDirectory(outputDir).
-		SetInitialSize(float64(initialSize)).
-		SetFinalSize(float64(finalSize))
-	result := resultBuilder.Build()
-	fmt.Println(result.PrintResults("grayscale"))
+	RunOperation(OperationConfig{
+		Ctx:                ctx,
+		FileSystem:         fs,
+		InputDir:           directory,
+		OutputSuffix:       "-grayscale",
+		ProgressBarMessage: "Creating grayscale images",
+		ProcessorFunc:      processGrayscaleImage,
+		ResultVerb:         "grayscale images created",
+	})
 }
 
 func processGrayscaleImage(ctx context.Context, params processing.FileProcessingParams, stats *utils.ImageProcessingStats) error {
-	select {
-	case <-ctx.Done():
-		return ctx.Err()
-	default:
-	}
-
-	img, err := params.FS.ReadFile(params.File.Path)
-	if err != nil {
-		atomic.AddUint32(stats.SkippedImages, 1)
-		return err
-	}
-
-	atomic.AddUint64(stats.InitialSize, uint64(params.File.Size))
-	newImg := image.NewBimgImage(img)
-	grayscaleImages, err := newImg.Grayscale()
-	if err != nil {
-		atomic.AddUint32(stats.SkippedImages, 1)
-		return err
-	}
-
-	outputPath := utils.BuildOutputPath(params.OutputDir, params.File.Path)
-	err = params.FS.WriteFile(outputPath, grayscaleImages)
-	if err != nil {
-		atomic.AddUint32(stats.SkippedImages, 1)
-		return err
-	}
-
-	atomic.AddUint64(stats.FinalSize, uint64(len(grayscaleImages)))
-	atomic.AddUint32(stats.ProcessedImages, 1)
-	return nil
+	return HandleImageProcessing(ctx, params, stats, func(img []byte) ([]byte, error) {
+		newImg := image.NewBimgImage(img)
+		return newImg.Grayscale()
+	})
 }
 
 var grayscaleCmd = &cobra.Command{
