1d7004375827131cbc61404221c17d9d129f923c
Felipe Simis <felipee.oliveira17@hotmail.com>
2026-04-30 10:19:48 -0300

    test(cmd): centralize image processor mock and command setup helper

---
+++ cmd/mock_processor_test.go
@@ -0,0 +1,46 @@
+package cmd
+
+import (
+	"bytes"
+
+	"github.com/felipesimis/go-compactify-cli/internal/filesystem"
+	"github.com/felipesimis/go-compactify-cli/internal/image"
+	"github.com/spf13/cobra"
+)
+
+type mockImageProcessor struct {
+	image.ImageProcessor
+	paletteCalled bool
+}
+
+func (m *mockImageProcessor) EnablePalette() ([]byte, error) {
+	m.paletteCalled = true
+	return []byte("fake-processed-bytes"), nil
+}
+
+type TestConfig struct {
+	FS            filesystem.FileSystem
+	MockProcessor *mockImageProcessor
+	OutBuf        *bytes.Buffer
+}
+
+func SetupTestConfig(createCmd func(filesystem.FileSystem, image.ProcessorFactory) *cobra.Command) (*cobra.Command, *TestConfig) {
+	fs := filesystem.NewFileSystem()
+	mockProcessor := &mockImageProcessor{}
+	outBuf := new(bytes.Buffer)
+
+	factory := func([]byte) image.ImageProcessor {
+		return mockProcessor
+	}
+
+	cmd := createCmd(fs, factory)
+	cmd.Flags().StringP("input", "i", "", "Input directory")
+	cmd.SetOut(outBuf)
+	cmd.SetErr(outBuf)
+
+	return cmd, &TestConfig{
+		FS:            fs,
+		MockProcessor: mockProcessor,
+		OutBuf:        outBuf,
+	}
+}
--- cmd/palette_test.go
+++ cmd/palette_test.go
@@ -1,48 +1,22 @@
 package cmd
 
 import (
-	"bytes"
 	"os"
 	"path/filepath"
 	"testing"
 
-	"github.com/felipesimis/go-compactify-cli/internal/filesystem"
-	"github.com/felipesimis/go-compactify-cli/internal/image"
 	"github.com/spf13/cobra"
 	"github.com/stretchr/testify/suite"
 )
 
-type mockImageProcessor struct {
-	image.ImageProcessor
-	called bool
-}
-
-func (m *mockImageProcessor) EnablePalette() ([]byte, error) {
-	m.called = true
-	return []byte("fake-processed-bytes"), nil
-}
-
 type PaletteTestSuite struct {
 	suite.Suite
-	fs            filesystem.FileSystem
-	cmd           *cobra.Command
-	outBuf        *bytes.Buffer
-	mockProcessor *mockImageProcessor
+	cmd    *cobra.Command
+	config *TestConfig
 }
 
 func (suite *PaletteTestSuite) SetupTest() {
-	suite.fs = filesystem.NewFileSystem()
-	suite.mockProcessor = &mockImageProcessor{}
-	mockFactory := func([]byte) image.ImageProcessor {
-		return suite.mockProcessor
-	}
-
-	suite.cmd = NewPaletteCmd(suite.fs, mockFactory)
-	suite.cmd.Flags().StringP("input", "i", "", "Input directory")
-
-	suite.outBuf = new(bytes.Buffer)
-	suite.cmd.SetOut(suite.outBuf)
-	suite.cmd.SetErr(suite.outBuf)
+	suite.cmd, suite.config = SetupTestConfig(NewPaletteCmd)
 }
 
 func (suite *PaletteTestSuite) TestPaletteShould_ReturnError_When_InputDirectoryDoesNotExist() {
@@ -60,7 +34,7 @@ func (suite *PaletteTestSuite) TestPaletteShould_Warn_When_DirectoryIsEmpty() {
 	err := suite.cmd.Execute()
 
 	suite.NoError(err)
-	suite.Contains(suite.outBuf.String(), "No files found in directory")
+	suite.Contains(suite.config.OutBuf.String(), "No files found in directory")
 }
 
 func (suite *PaletteTestSuite) TestPaletteShould_ProcessImageSuccessfully() {
@@ -72,9 +46,9 @@ func (suite *PaletteTestSuite) TestPaletteShould_ProcessImageSuccessfully() {
 	err := suite.cmd.Execute()
 
 	suite.NoError(err)
-	suite.True(suite.mockProcessor.called, "enablePalette should have been called")
+	suite.True(suite.config.MockProcessor.paletteCalled, "enablePalette should have been called")
 
-	output := suite.outBuf.String()
+	output := suite.config.OutBuf.String()
 	suite.Contains(output, "1 images")
 	suite.Contains(output, "Processed")
 }
