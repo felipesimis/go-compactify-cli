dd8abc914095600f69bc46415d10d639c7386ba4
Felipe Simis <felipee.oliveira17@hotmail.com>
2026-04-30 10:46:28 -0300

    test(cmd): add functional tests for lossless command execution

--- cmd/lossless_test.go
+++ cmd/lossless_test.go
@@ -1,6 +1,8 @@
 package cmd
 
 import (
+	"os"
+	"path/filepath"
 	"testing"
 
 	"github.com/spf13/cobra"
@@ -23,6 +25,29 @@ func (suite *LosslessTestSuite) TestLosslessShould_ReturnError_When_InputDirecto
 
 	suite.Error(err)
 	suite.Contains(err.Error(), "failed to open directory")
+}
+
+func (suite *LosslessTestSuite) TestLosslessShould_Warn_When_DirectoryIsEmpty() {
+	tmpDir := suite.T().TempDir()
+
+	suite.cmd.SetArgs([]string{"--input", tmpDir})
+	err := suite.cmd.Execute()
+
+	suite.NoError(err)
+	suite.Contains(suite.config.OutBuf.String(), "No files found in directory")
+}
+
+func (suite *LosslessTestSuite) TestLosslessShould_ProcessImageSuccessfully() {
+	tmpDir := suite.T().TempDir()
+	testPath := filepath.Join(tmpDir, "test.jpg")
+	suite.Require().NoError(os.WriteFile(testPath, []byte("fake"), 0644))
+
+	suite.cmd.SetArgs([]string{"--input", tmpDir})
+	err := suite.cmd.Execute()
+
+	suite.NoError(err)
+	suite.True(suite.config.MockProcessor.losslessCalled)
+	suite.Contains(suite.config.OutBuf.String(), "Processed")
 }
 
 func TestLosslessSuite(t *testing.T) {
